fdf50052a66b5216ecaa88f3f6a81202481ee163
manjukalal <manjukalal1496@gmail.com>
2022-06-30 16:29:50 +0530

    Added:firstname to user registration

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -4,6 +4,7 @@ public class RegexMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
-
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.firstName();
     }
 }
